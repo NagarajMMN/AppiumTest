3469a2d8e1453d40a49699f6e02642ef1ff1cb6c
NagarajMMN <nagarajm8300@gmail.com>
2023-04-25 11:44:02 +0530

    My first commit

--- src/test/java/AppiumSampleTest/BaseTest.java
+++ src/test/java/AppiumSampleTest/BaseTest.java
@@ -33,7 +33,33 @@ public class BaseTest {
 				ImmutableMap.of("elementId",((RemoteWebElement)ele).getId(),
 						"duration",2000));
 	}
-	
+
+	public void ScrollToEndAction()	{
+		boolean canScrollMore;
+		do
+		{
+			canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+					"left", 100, "top", 100, "width", 200, "height", 200,
+					"direction", "down",
+					"percent", 3.0
+
+			));
+		}while(canScrollMore);
+	}
+
+	public void swipeAction(WebElement ele,String direction)
+	{
+		((JavascriptExecutor) driver).executeScript("mobile: swipeGesture", ImmutableMap.of(
+				"elementId", ((RemoteWebElement)ele).getId(),
+
+				"direction", direction,
+				"percent", 0.75
+		));
+
+
+	}
+
+
 	@AfterClass
 	public void TearDown() {
 
